e94f0683c0c52f2c3843634e6f2e37ab5c64a34e
infinityMark <jiachengwu14@gmail.com>
2026-09-16 15:30:10 +0800

    # Complete jumping mario - Start circle game

---
+++ week3/Circle Game/LinkNode.h
@@ -0,0 +1,16 @@
+#ifdef LISTNODE_H
+#define LISTNODE_H
+
+class ListNode{
+    public:
+        ListNode(int);
+        ListNode(int, ListNode*);
+        ListNode* getNext() {return next;};
+        int getData() {return data;};
+        void setNext(ListNode* n) {next = n;};
+    private:
+        int data;
+        ListNode* next;
+}
+
+#endif
--- week3/Jumping Mario.cpp
+++ week3/Jumping Mario.cpp
@@ -18,25 +18,21 @@ int main(){
         for (int i = 0; i < lengths; ++i) {
             cin >> walls[i]; // Read each wall height
         }
-		
-		if(lengths == 1) { return 0 ;};
 	
-		// Not signle walls
-		int highJump = 0;
-		int lowJump = 0;
+		int highJump = 0, lowJump = 0;
 		int currentWalls = walls[0];
-		
-		
-		for (int i = 1; i < lengths -1 ; i++){  // set conditon to -1 to prevent over flow
+
+   // if the length == 1 also, they it can not pass the for loop conditon check. so no need write a special case for it
+		for (int i = 1; i < lengths; i++){  
 			// each i represents height of next wall
-			if (currentWalls < walls[i+1]){
+			if (currentWalls < walls[i]){
 				highJump++;
-			} else if(currentWalls > walls[i+1]){
+			} else if(currentWalls > walls[i]){
 				lowJump++;
 			}
 			currentWalls = walls[i];
 		}
-		cout << "Case " << testCase << ": " << highJump << " " << lowJump << "\n";
+		cout << "Case " << testCase + 1<< ": " << highJump << " " << lowJump << "\n";
 	}
 	
 	return 0;
